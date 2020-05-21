05caf12c98a59d1fde7f78b5566e733ea4db17fc
sergiu nimat <stud.demy@gmail.com>
2020-05-21 23:57:11 +0200

    Customer menus and log out functionality

--- app/src/main/java/com/example/orderquick/MainCustomerView.java
+++ app/src/main/java/com/example/orderquick/MainCustomerView.java
@@ -1,15 +1,21 @@
 package com.example.orderquick;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainCustomerView extends AppCompatActivity {
+public class MainCustomerView extends BaseAppClass {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -52,5 +58,36 @@ public class MainCustomerView extends AppCompatActivity {
             }
         });
                 viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+
+
+    /*I: Function to inflate the customer menu for the user.*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater i = getMenuInflater();
+        i.inflate(R.menu.custommer_menu,menu);
+        return true;
+    }
+
+    /*I: Menu items on click listener*/
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.customer_profile_id:
+                Toast.makeText(this, "For this you need to redirect the user to its profile and allow him/her to modify ", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.customer_orders_id:
+                Toast.makeText(this, "Create and show the list of orders the user has if null show message there isnt any order yet", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.customer_logout_id:
+                //set the current user to null and redirect to login
+                APPLICATION_CURRENT_USER =null;
+                Intent login = new Intent(this,LogIn.class);
+                startActivity(login);
+                //deny user to go back to previous activity once is logged out.
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
